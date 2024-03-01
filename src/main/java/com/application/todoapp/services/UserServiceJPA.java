d17b7c08a2c052d19fef9d84bdd058f4d6e0b05a
Vermillion <nhatlx159@gmail.com>
2024-03-01 18:21:09 +0700

    add hashing password function for security

--- src/main/java/com/application/todoapp/services/UserServiceJPA.java
+++ src/main/java/com/application/todoapp/services/UserServiceJPA.java
@@ -10,10 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -46,7 +47,41 @@ public class UserServiceJPA implements UserService {
                         throw new RuntimeException(e);
                     }
                 });
+        // Hash password
+        String password = userDTO.getPassword();
+        byte[] salt = generateSalt();
+
+        // Generate hashed password
+        String hashedPassword = hashPassword(password, salt);
+        userDTO.setPassword(hashedPassword);
+        // Validate a password
+//        boolean isValid = validatePassword(password, salt, hashedPassword);
+//        if(!isValid){
+//            return null;
+//        }
+
         return userMapper.userEntityToUserDto(userRepository.save(userMapper.userDtoToUserEntity(userDTO)));
+    }
+    private byte[] generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+    private String hashPassword(String password, byte[] salt){
+        try {
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    private boolean validatePassword(String inputPassword, byte[] salt, String hashedPassword){
+        String newlyHashedPassword = hashPassword(inputPassword, salt);
+        return hashedPassword.equals(newlyHashedPassword);
     }
 
     @Override
